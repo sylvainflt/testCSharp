fddf78d516058e14bcfe0e15251f6ed027d7021b
sylvainflt <sylvainfoucault1@gmail.com>
2022-12-12 15:28:33 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom = "jean";
+            string? saisie = "";
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bonjour le monde!");
+
+            while(saisie != nom){
+                try
+                {
+                    saisie = Console.ReadLine();
+                    
+                }
+                catch (IOException e)
+                {
+                    TextWriter errorWriter = Console.Error;
+                    errorWriter.WriteLine(e.Message);
+                }
+            }     
+            Console.WriteLine("Bonjour "+saisie);       
+
+        }
+    }
+}
